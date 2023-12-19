341febeff6959272fafc3c54e3397805d4be3c93
OliveiraGabrielDev <gabrielsouzacontato.99@gmail.com>
2023-12-19 07:35:50 -0300

    Initial commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { Head } from '@inertiajs/vue3'
+
+defineProps({ user: String })
+</script>
+
+<template>
+    <Head title="Teste" />
+    <p>Hello {{ user }}, welcome to your first Inertia app!</p>
+</template>
